c1db5ec3d064dbea0e36103617500de7a59ebab4
Denis Stepulenok <super.denis@gmail.com>
2015-01-30 01:38:33 +0300

    Окно с Runtime Error для .NET

---
+++ VisualBasicBug/check_runtime.cmd
@@ -0,0 +1,5 @@
+echo Visual Basic (.NET 4.5)
+SET PATH="C:\Windows\Microsoft.NET\Framework\v4.0.30319";%PATH%
+vbc runtime.bas
+runtime.exe
+
---
+++ VisualBasicBug/runtime.bas
@@ -0,0 +1,7 @@
+Module Module1
+    Sub Main
+        Dim d, f As Integer
+        d = 0
+        f = 3 / d
+    End Sub
+End Module
